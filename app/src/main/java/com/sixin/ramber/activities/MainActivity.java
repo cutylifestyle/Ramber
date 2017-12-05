805e7edd1260e4f3f1f54ee74e356573d952ee5d
20553 <2055305346@qq.com>
2017-12-05 21:51:20 +0800

    主界面的碎片部分的搭建

--- app/src/main/java/com/sixin/ramber/activities/MainActivity.java
+++ app/src/main/java/com/sixin/ramber/activities/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     //TODO 侧滑抽屉部分划出的时候没有响应,是因为布局顺序颠倒的原因，需要看源码
     //TODO drawable文件夹命名的秘密，以及style文件夹命名的秘密
     //TODO 矢量图
+    //TODO 碎片源码分析   CoordinatorLayout AppBarLayout ToolBar
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/sixin/ramber/fragments/MainFragment.java
@@ -0,0 +1,27 @@
+package com.sixin.ramber.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sixin.ramber.R;
+
+/**
+ * @author zhou
+ */
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+}
